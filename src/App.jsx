59279c383121adde893f3622a868db84ceb64fc9
Isak La Fleur <isak.engdahl@gmail.com>
2017-04-20 11:01:13 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
+
+const Links = () => (
+  <nav>
+    <Link to="/?id=123">Inline</Link>
+    <Link to={{ pathname: '/', search: 'id=456' }}>Object</Link>
+  </nav>
+);
 
 const App = (props) => (
   <Router>
     <div>
+      <Links />
       <Route
-        path="/:page?-:subpage?" render={({match}) => (
-          <h1>
-          PAGE: { match.params.page || 'Home' }<br />
-          SUBPAGE: { match.params.subpage }
-          </h1>
-          )}
+        path="/" render={({match, location}) => (
+          <div>
+            <p>root</p>
+            <p>{JSON.stringify(match)}</p>
+            <p>{JSON.stringify(location)}</p>
+            <p>{new URLSearchParams(location.search).get('id')}</p>
+          </div>
+      )}
       />
     </div>
   </Router>
